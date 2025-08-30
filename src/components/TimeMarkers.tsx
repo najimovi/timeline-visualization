579b673171260db9e5241f2c112ef1793bd3c960
najimovi <pablo.najimovich@gmail.com>
2025-08-30 17:48:27 -0300

    fix: axis labels readability

--- src/components/TimeMarkers.tsx
+++ src/components/TimeMarkers.tsx
@@ -14,10 +14,10 @@ export default function TimeMarkers({ timeMarkers }: TimeMarkersProps) {
       {timeMarkers.months.map((marker, index) => (
         <div
           key={`month-${index}`}
-          className="border-border absolute bottom-0 h-[5rem] border-l-8"
+          className="border-border absolute bottom-0 h-[5.5rem] border-l-8"
           style={{ left: `${marker.position}%` }}
         >
-          <span className="text-foreground absolute top-1 left-2 text-sm font-semibold">
+          <span className="text-foreground absolute top-1 left-2 text-sm font-bold">
             {formatMonthYear(marker.date)}
           </span>
         </div>
@@ -32,7 +32,7 @@ export default function TimeMarkers({ timeMarkers }: TimeMarkersProps) {
         >
           <span
             className={twMerge(
-              'text-muted-foreground absolute top-1 left-1 rotate-45 text-[14px] whitespace-nowrap select-none',
+              'text-muted-foreground absolute bottom-2 left-1 mb-2 rotate-45 text-[16px] font-semibold whitespace-nowrap select-none',
               // zoomLevel < 1 ? 'text-[10px]' : 'text-[14px]',
             )}
           >
